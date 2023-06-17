238f0bb4ff159c97b4400bdb9708ba85e9698f77
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-06-17 16:11:35 +0500

    adding router

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ return new Router([
         'GET' => fn($data) => array_merge([
             'message' => 'Hello world'
         ], $data)
-    ]
+    ],
+    '/about' => [
+        'GET' => fn() => [
+            'message' => 'About us'
+        ]
+    ],
 ]);
