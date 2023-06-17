135bda9278b2de20c85f2338594da62fcab904f8
khamdullaevuz <elbek.khamdullaev@gmail.com>
2023-06-18 00:25:16 +0500

    exception customized

--- public/index.php
+++ public/index.php
@@ -4,13 +4,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $router = require __DIR__ . '/../routes/web.php';
 
 use Khamdullaevuz\Framework\App;
-use Khamdullaevuz\Framework\Exceptions\InvalidInstanceException;
 use Khamdullaevuz\Framework\Http\Kernel;
 use Khamdullaevuz\Framework\Http\Request;
 
 try {
     $kernel = App::make(Kernel::class, $router);
-} catch (InvalidInstanceException $e) {
+} catch (Exception $e) {
     die($e->getMessage());
 }
 
